5dd9d4f496d17449fca125261eb9da2337b4387b
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2023-02-14 11:57:18 +0300

    added Html and styled it using css

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,28 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing{
-    public static function all(){
-        return [
-            [
-                'id'=>1,
-                'title'=>'listing 1',
-                'description'=>'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel'
-            ],
-            [
-                'id'=>2,
-                'title'=>'listing 2',
-                'description'=>'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel'
-            ]
-             ];
-    }
-    public static function find($id){
-       $listings=self::all();
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-       foreach($listings as $listing){
-        if($listing['id']==$id){
-            return $listing;
-        }
-       }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
